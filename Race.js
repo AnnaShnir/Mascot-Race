650734b0d4d4e032ddc75d469bb00c559a57cc29
Anna Shnir <shnir.anna@gmail.com>
2015-03-23 16:23:05 -0400

    Constructor here

---
+++ Race.js
@@ -0,0 +1,16 @@
+
+var Mascot = function(name, food, endurance, tripFactor) {
+    this.name = name;
+    this.food = food;
+    this.endurance = endurance;
+    this.tripFactor = tripFactor;
+
+}
+
+var badger = new Mascot("Badger", "Bagel", 3, false);
+var vixen = new Mascot("Vixen", "V8", 3, false);
+var unicorn = new Mascot("Unicorn", "Udon", 4, true);
+
+if (this.tripFactor === true) {
+    
+}
